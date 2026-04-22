496df4edeb9963082d598fb4df62b64bd766406d
Vikranth V <vvikranth08@gmail.com>
2026-04-22 08:23:18 +0530

    Create 8) Subnetting.java

---
+++ 8) Subnetting.java
@@ -0,0 +1,10 @@
+public class Subnetting {
+    public static void main(String[] args) {
+        String ip = "192.168.1.0";
+        int subnets = 4;
+
+        System.out.println("IP: " + ip);
+        System.out.println("Subnets: " + subnets);
+        System.out.println("Each subnet has 62 hosts");
+    }
+}
