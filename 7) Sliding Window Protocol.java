9883cf148a91ceb357e7db1f31c5192649e61ef8
Vikranth V <vvikranth08@gmail.com>
2026-04-28 10:38:18 +0530

    Update 7) Sliding Window Protocol.java

--- 7) Sliding Window Protocol.java
+++ 7) Sliding Window Protocol.java
@@ -1,16 +1,54 @@
-import java.util.*;
-
 class SlidingWindow {
-    public static void main(String[] args) {
-        int windowSize = 3;
-        int[] frames = {1,2,3,4,5};
 
-        for(int i=0;i<frames.length;i+=windowSize){
-            System.out.print("Sending: ");
-            for(int j=i;j<i+windowSize && j<frames.length;j++){
-                System.out.print(frames[j]+" ");
-            }
-            System.out.println("\nACK received");
+    private int totalFrames;
+    private int windowSize;
+    private int sent;
+
+    SlidingWindow(int totalFrames, int windowSize) {
+        this.totalFrames = totalFrames;
+        this.windowSize = windowSize;
+        this.sent = 0;
+    }
+
+    void sendFrames() {
+        System.out.print("Sender: Sending frames: ");
+        for (int i = sent; i < sent + windowSize && i < totalFrames; i++) {
+            System.out.print((i + 1) + " ");
         }
+        System.out.println();
+    }
+
+    void receiveFrames() {
+        System.out.print("Receiver: Received frames: ");
+        for (int i = sent; i < sent + windowSize && i < totalFrames; i++) {
+            System.out.print((i + 1) + " ");
+        }
+        System.out.println();
+    }
+
+    void sendAck() {
+        int ack = Math.min(sent + windowSize, totalFrames);
+        System.out.println("Receiver: Sending ACK for frame " + ack);
+    }
+
+    void slideWindow() {
+        sent += windowSize;
+        System.out.println("Sender: Window Slid!\n");
+    }
+
+    void start() {
+        while (sent < totalFrames) {
+            sendFrames();
+            receiveFrames();
+            sendAck();
+            slideWindow();
+        }
+    }
+}
+
+class SlidingWindowProtocol {
+    public static void main(String[] args) {
+        SlidingWindow sw = new SlidingWindow(7, 3);
+        sw.start();
     }
 }
